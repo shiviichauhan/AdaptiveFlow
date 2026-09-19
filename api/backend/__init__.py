b337df45b13747734b2a529c5e32c586f0f10e09
shiviichauhan <shivanich0015@gmail.com>
2026-09-19 13:52:30 +0530

    Delete api directory

--- api/backend/__init__.py
+++
@@ -1,5 +0,0 @@
-from .models import *
-from .knowledge_graph import KnowledgeGraph, create_sample_knowledge_graph
-from .learner_model import LearnerModel
-from .recommendation_engine import RecommendationEngine
-from .evaluation import EvaluationSystem
